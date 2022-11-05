a3b808db9f502772db94cad4402c5196e8e8c5ee
Ali <alivaraste@outlook.com>
2022-11-05 20:33:52 +0330

    style update

--- Components/Section.styles.js
+++ Components/Section.styles.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
   padding: 10px 30px;
   margin: 10px;
   border-radius: 5px;
   background-color: white;
   cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 
   .group {
     display: flex;
@@ -17,5 +17,16 @@ export const Container = styled.div`
     background-color: #e0e0e0;
     width: 20px;
     height: 20px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  margin: 10px;
+  .item {
+    display: flex;
+    justify-content: space-around;
+    padding: 10px;
+    margin-top: 10px;
+    background-color: #a0a0a0;
   }
 `;
